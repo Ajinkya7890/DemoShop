db937cdc3ed4b98c328d140ed7a1d6b935b0e4d9
Ajinkya7890 <161580551+Ajinkya7890@users.noreply.github.com>
2026-07-25 20:07:38 +0530

    feat: integrate SQLAlchemy and create user model

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
 
 from app.routes import main
+from app.database.db import db
 from app.config.logging_config import logger
+
+# Import models so SQLAlchemy knows about them
+from app.models.user import User
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///demoshop.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(main)
 
